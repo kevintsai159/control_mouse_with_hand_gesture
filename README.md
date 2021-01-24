2ae2167713f50e1c936c187734643bc244f1c242
Kevin Tsai <kevintsai159@gmail.com>
2021-01-25 00:16:48 +0800

    main.py: Stabilize the mouse pointer. README.md: Add more instruction

--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 
 ## Description:
 
-The purpose of this project is to control your PC's mouse based on your hand gesture. This allows you to control the mouse (on display) without the mouse (physically). :)
+The purpose of this project is to control your PC's mouse based on your hand gesture. This allows you to control the mouse (on display) without the mouse (physical). :)
+
+Currently, the mouse is controlled based on index fingertip of one hand. The program can only detects one hand at a time. Multi-hand detection and other functions (mouse click, double click, right click, etc.) will be implemented later on.
+
+More detail of the program will be added later on.
 
 ## Requirements:
 - Programming Language:
-	- Python 3.6
+	- Python 3.x
 - Packages:
 	- OpenCV
 	- MediaPipe
-- Hardwares:
-	- Webcam (Higher resolution gives you higher accuracy)
+- Hardware:
+	- Webcam
 
 ## Instructions:
-Coming soon
+1. Place your webcam on the top of the monitor and facing you for best result.
+2. Run main.py.
+3. A window named 'Hands Gesture Detection' will show up.
+4. While controlling the mouse with hand gesture, do not place your hand too close to the webcam. (This will be implemented later on to overcome the issue when hand(s) are too close to the webcam.)
+5. To terminate the program, to back to the 'Hands Gesture Detection' window and press ESC.
+4. Enjoy~
--- main.py
+++ main.py
@@ -8,36 +8,51 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# pyautogui.FAILSAFE = False
+
 # Get your main screen resolution
 resolution = pyautogui.size()
 screen_width = resolution.width
 screen_height = resolution.height
 
-# Load MediaPipe hands model
+# Load MediaPipe 'hands' model
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 # For webcam input:
-hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-cap = cv2.VideoCapture(0)
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.9, max_num_hands=1)
+cap = cv2.VideoCapture(0)  # Use the first webcam from your PC
 cap.set(3, screen_width)
 cap.set(4, screen_height)
 cap_width = int(cap.get(3))
 cap_height = int(cap.get(4))
 
-
 # Set Variables
-cap_boundary = 50
-mouse_x = 50
-mouse_y = 50
-mouse_x_pre = 50
-mouse_y_pre = 50
-mouse_count = 0
-mouse_avg_x = 0
-mouse_avg_y = 0
-mouse_avg_x_move = 0
-mouse_avg_y_move = 0
-mouse_locations = []
+cap_boundary = 150  # Set a boundary from the edge of the frame since the hand detection accuracy is low around the edge
+mouse_ptr = np.array([50, 50], dtype=np.int)  # Mouse x,y position
+mouse_ptr_pre = mouse_ptr.copy()
+mouse_ptr_diff = np.array([0, 0], dtype=np.int)
+mouse_stable_count = 0
+
+# Debugging
+record_test_x = []
+record_test_y = []
+
+# stable mark
+stable_count = 5
+stable_lamda = 0.8
+stable_discount = [stable_lamda]
+stable_remain = 1
+for i in range(stable_count - 2):
+    stable_remain -= stable_discount[-1]
+    stable_discount += [stable_remain * stable_lamda]
+stable_discount += [1 - sum(stable_discount)]
+stable_discount.reverse()
+stable_ptr = []
 
 while cap.isOpened():
     success, image = cap.read()
@@ -46,54 +61,58 @@ while cap.isOpened():
         # If loading a video, use 'break' instead of 'continue'.
         continue
 
-    # Flip the image horizontally for a later selfie-view display, and convert
-    # the BGR image to RGB.
+    # Flip the image horizontally for a later selfie-view display, and convert the BGR image to RGB.
     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
+    # To improve performance, optionally mark the image as not writeable to pass by reference.
     image.flags.writeable = False
     results = hands.process(image)
-
     # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
+        # Index fingertip pointer [x,y]
+        ft_index = [results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x,
+                    results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y]
+        # Stabilize the pointer
+        stable_ptr += [ft_index]
+        stable_ptr = stable_ptr[-stable_count:]
+        ft_index = [0, 0]
+        for i in range(len(stable_ptr)):
+            ft_index[0] += stable_ptr[-i - 1][0] * stable_discount[-i - 1]
+            ft_index[1] += stable_ptr[-i - 1][1] * stable_discount[-i - 1]
+        ft_index[0] += stable_ptr[-len(stable_ptr)][0] * (1 - sum(stable_discount[-len(stable_ptr):]))
+        ft_index[1] += stable_ptr[-len(stable_ptr)][1] * (1 - sum(stable_discount[-len(stable_ptr):]))
+        results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x = ft_index[0]
+        results.multi_hand_landmarks[0].landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y = ft_index[1]
+
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
                 image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         # Set a boundary for finger detection range since when hand(s) is at position near the edge,
         # hand detection accuracy drops. We have to make sure the algorithm detects the hand.
-        finger_x = min(max(results.multi_hand_landmarks[0].landmark[
-                               mp_hands.HandLandmark.INDEX_FINGER_TIP].x * cap_width - cap_boundary, 0),
-                       cap_width - 2 * cap_boundary) / (cap_width - 2 * cap_boundary)
-        finger_y = min(max(results.multi_hand_landmarks[0].landmark[
-                               mp_hands.HandLandmark.INDEX_FINGER_TIP].y * cap_height - cap_boundary, 0),
-                       cap_height - 2 * cap_boundary) / (cap_height - 2 * cap_boundary)
-        print(finger_x, finger_y)
-        mouse_x = finger_x * screen_width
-        mouse_y = finger_y * screen_height
-        mouse_avg_x_move += mouse_x - mouse_x_pre
-        mouse_avg_y_move += mouse_y - mouse_y_pre
-        if abs(mouse_x - mouse_x_pre) <= 10 and abs(
-                mouse_y - mouse_y_pre) <= 10 and mouse_avg_x_move <= 10 and mouse_avg_y_move <= 10:
-            mouse_count += 1
-            if mouse_count == 20:
-                mouse_count = 0
-                pyautogui.click(x=mouse_x, y=mouse_y)
-                mouse_avg_x_move = 0
-                mouse_avg_y_move = 0
+        ft_index = [min(max(ft_index[0] * cap_width - cap_boundary, 0),
+                        cap_width - 2 * cap_boundary) / (cap_width - 2 * cap_boundary),
+                    min(max(ft_index[1] * cap_height - cap_boundary, 0),
+                        cap_height - 2 * cap_boundary) / (cap_height - 2 * cap_boundary)]
+        mouse_ptr[:] = [ft_index[0] * screen_width, ft_index[1] * screen_height]
+        mouse_ptr_diff += mouse_ptr - mouse_ptr_pre
+        if np.where(abs(mouse_ptr - mouse_ptr_pre) > 10)[0].size == 0 and np.where(mouse_ptr_diff > 10)[0].size == 0:
+            mouse_stable_count += 1
+            if mouse_stable_count == 20:
+                mouse_stable_count = 0
+                mouse_ptr_diff[:] = [0, 0]
+                # pyautogui.click(x=mouse_x, y=mouse_y)     # Mouse click
         else:
-            mouse_avg_x_move = 0
-            mouse_avg_y_move = 0
-            mouse_count = 0
-        # print(mouse_count, mouse_avg_x_move, mouse_avg_y_move)
-        mouse_x_pre = mouse_x
-        mouse_y_pre = mouse_y
-        pyautogui.moveTo(mouse_x, mouse_y, _pause=False)
+            mouse_ptr_diff[:] = [0, 0]
+            mouse_stable_count = 0
 
+        mouse_ptr_pre[:] = mouse_ptr[:]
+        pyautogui.moveTo(mouse_ptr[0], mouse_ptr[1], _pause=False)
+
+    # Show the boundary with a red rectangle
     cv2.rectangle(image, (cap_boundary, cap_boundary), (cap_width - cap_boundary, cap_height - cap_boundary),
                   color=(0, 0, 255), thickness=5)
-    cv2.imshow('MediaPipe Hands', image)
+    cv2.imshow('Hands Gesture Detection', image)
     if cv2.waitKey(1) & 0xFF == 27:
         break
 hands.close()
